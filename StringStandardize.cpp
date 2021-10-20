79acc0d176e0d179395577ee186ecc2807017928
Khoa Nguyen <81000230+thoaikhoa14402@users.noreply.github.com>
2021-10-20 14:57:18 +0700

    Update StringStandardize.cpp

--- StringStandardize.cpp
+++ StringStandardize.cpp
@@ -54,3 +54,13 @@ int main()
     cout << s;
     return 0;
 }
+
+// method 2
+string Standardize(string s) {
+	string temp;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] >= 'A' && s[i] <= 'Z') s[i] += 32;
+		if (s[i] >= 'a' && s[i] <= 'z') temp += s[i];
+	}
+	return temp;
+}
